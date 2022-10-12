2b2e8b896f767d677ee1bc8e99f14d2b22c8775f
renyopaulo <renyopaulo@gmail.com>
2022-10-12 18:34:47 -0300

    Using whenAll

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Apis.Discovery.v1;
 using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using System.Diagnostics;
@@ -10,11 +11,13 @@ namespace gmail_netcore_pof
     {
         const string apiKey = "AIzaSyAyLBX_GK6vFFqhUOxJ__NY6e7n2rMwk38";
         const string hostEmailAddress = "me";
+        public static GmailService service = new GmailService();
+        public static List<String> list = new List<string>();
         static async Task Main(string[] args)
         {
             Console.WriteLine("Discovery API Sample");
             Console.WriteLine("====================");
-
+            service = await GmailAPIHelper.GetService();
             var timer = new Stopwatch();
             timer.Start();
 
@@ -69,114 +72,78 @@ namespace gmail_netcore_pof
                 }
             }
         }
-
+ 
         private async Task<string> GetMessages(string? nextPageToken = null)
         {
-            var service = await GmailAPIHelper.GetService();
             // Run the request.
             Console.WriteLine("Executing a list request...");
             var msgList = service.Users.Messages.List(hostEmailAddress);
             msgList.LabelIds = "INBOX";
             msgList.Q = "is:read";
-            msgList.MaxResults = 30;
+            msgList.MaxResults = 50;
             var result = await msgList.ExecuteAsync();
             var newNextPageToken = result.NextPageToken;
             // Estimated total number of results.
             var estimated = result.ResultSizeEstimate;
 
-            int counter = 0;
             // Display the results.
             // List of messages. Note that each message resource contains only an `id` and a `threadId`. 
             // Additional message details can be fetched using the messages.get
-
-            var list = new List<string>();
+            
+            var tasksList = new List<Task>(result.Messages.Count);
             if (result.Messages != null)
             {
-                ParallelLoopResult r = Parallel.ForEach(result.Messages, async msg =>
-                                {
-                                    counter++;
-                                    //MESSAGE MARKS AS READ AFTER READING MESSAGE
 
-                                    ModifyMessageRequest mods = new();
-                                    mods.AddLabelIds = null;
-                                    mods.RemoveLabelIds = new List<string> { "UNREAD" };
-                                    service.Users.Messages.Modify(mods, hostEmailAddress, msg.Id).Execute();
+                //ThreadPool.QueueUserWorkItem()
 
-
-                                    var Message = service.Users.Messages.Get(hostEmailAddress, msg.Id);
-                                    //Console.WriteLine($"-----------------NEW MAIL:{counter}----------------------");
-                                    //Console.WriteLine("Message ID:" + msg.Id);
-
-                                    //MAKE ANOTHER REQUEST FOR THAT EMAIL ID...
-                                    Message msgContent = await Message.ExecuteAsync();
-
-                                    if (msgContent != null)
-                                    {
-                                        string fromAddress = string.Empty;
-                                        string date = string.Empty;
-                                        string subject = string.Empty;                                        
-                                        string mailBody = string.Empty;
-                                        string readableText = string.Empty;
-
-                                        // fromAddress = msgContent.Payload.Headers.First(h => h.Name == "From").Value;
-                                        // date = msgContent.Payload.Headers.First(h => h.Name == "Date").Value;
-                                        // date = msgContent.Payload.Headers.First(h => h.Name == "Subject").Value;
-
-                                        //LOOP THROUGH THE HEADERS AND GET THE FIELDS WE NEED (SUBJECT, MAIL)
-                                        foreach (var msgParts in msgContent.Payload.Headers)
-                                        {
-                                            if (msgParts.Name == "From")
-                                            {
-                                                fromAddress = msgParts.Value;
-                                            }
-                                            else if (msgParts.Name == "Date")
-                                            {
-                                                date = msgParts.Value;
-                                            }
-                                            else if (msgParts.Name == "Subject")
-                                            {
-                                                subject = msgParts.Value;
-                                                list.Add(subject);
-                                            }
-                                        }
-
-
-                                        // mailBody = string.Empty;
-                                        // if (msgContent.Payload.Parts == null && msgContent.Payload.Body != null)
-                                        // {
-                                        //   mailBody = msgContent.Payload.Body.Data;
-                                        // }
-                                        // else if (msgContent.Payload.Parts != null)
-                                        // {
-                                        //   mailBody = GmailAPIHelper.MsgNestedParts(msgContent.Payload.Parts);
-                                        // }
-
-                                        // //BASE64 TO READABLE TEXT--------------------------------------------------------------------------------
-                                        // readableText = string.Empty;
-                                        // readableText = GmailAPIHelper.Base64Decode(mailBody);
-
-                                        //Console.WriteLine($"fromAddress: {fromAddress}");
-                                        //Console.WriteLine($"date: {date}");
-                                        //Console.WriteLine($"subject: {subject}");
-                                        // Console.WriteLine($"mailBody: {mailBody}");
-                                        // Console.WriteLine($"readableText: {readableText}");
-
-                                    }
-                                });
-                Console.WriteLine("Waiting");
-                Console.WriteLine("Completou ? " + r.IsCompleted);
-                if (r.IsCompleted)
-                {
-                    Console.WriteLine("COMPLETADO");
-                    foreach (var item in list)
+                 foreach (var msg in result.Messages)
+                 {
+                    var task = Task.Run(async () =>
                     {
-                        Console.WriteLine(item);
-                    }
+                        Console.WriteLine("Application thread ID: {0}",
+                                                System.Threading.Thread.CurrentThread.ManagedThreadId);
+                        try
+                        {
+                            Message m = (Message)msg;
+                            var Message = service.Users.Messages.Get(hostEmailAddress, m.Id);
+                            Message msgContent = await Message.ExecuteAsync();
+                            string subject = msgContent.Payload.Headers.FirstOrDefault(h => h.Name == "Subject").Value;
+                            list.Add(subject);
+                        }
+                        catch (Google.GoogleApiException e)
+                        {                            
+                            if (e.HttpStatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                            {
+                                Console.WriteLine("Ooppss");
+                            }
+                        }
+                        await Task.Delay(100);
+                    });
+                    tasksList.Add(task);
+                 }
+
+                await Task.WhenAll(tasksList.ToArray());
+
+                
+
+               /* foreach (var msg in result.Messages)
+                {
+                    var Message = service.Users.Messages.Get(hostEmailAddress, msg.Id);
+                    Message msgContent = await Message.ExecuteAsync();
+                    string subject = msgContent.Payload.Headers.FirstOrDefault(h => h.Name == "Subject").Value;
+                    Console.WriteLine(subject);
+                    list.Add(subject);
+
+                }*/
+
+                Console.WriteLine(list.Count);
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
                 }
             }
-            
+
             return newNextPageToken;
         }
-
     }
 }
